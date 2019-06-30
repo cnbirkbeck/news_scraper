2c391a3b59deccf4eb33cad2cf41a54127d84f3f
CBirkbeck <cnbirkbeck@gmail.com>
2019-06-30 16:23:01 -0700

    updated article.js with models

---
+++ models/article.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create schema
+const ArticlSchema = new Schema ({
+    title: {
+        type: String,
+        required: true, 
+        unique: true
+    },
+    link:{
+        type: String,
+        required: true, 
+        unique: true
+    },
+    summary: {
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    note: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "note"
+        }
+    ]
+});
+
+// Create model from schema
+const Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
